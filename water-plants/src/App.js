7170b770ce50136022fef4e0367a94154232b234
charlene-johnson <charlene.j8234@gmail.com>
2020-05-21 17:16:27 -0400

    Accidently wrote code into the wrong component, copied code into the correct component

--- water-plants/src/App.js
+++ water-plants/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import LoginForm from "./components/LoginForm"
+import SignUpForm from "./components/SignUpForm";
 
 function App() {
   return (
     <div className="App">
-      <LoginForm/>
+      <SignUpForm/>
     </div>
   );
 }
