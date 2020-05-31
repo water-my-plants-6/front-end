753e6b50822a29e9fd3083eec6d7d3fa19c22517
marissa-shaffer <marissa.shaffer1@gmail.com>
2020-05-31 16:38:45 -0500

    Set up  store in index

--- water-plants/src/index.js
+++ water-plants/src/index.js
@@ -4,15 +4,19 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter as Router} from "react-router-dom";
+import {Provider} from "react-redux";
+import  { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { reducer } from "./components/store/reducer/index";
 
+const store = createStore(reducer, applyMiddleware(thunk));
+
+const rootElement = document.getElementById('root');
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
+  <Provider store={store}>
     <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  </Provider> 
+, rootElement);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
